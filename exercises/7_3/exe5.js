b3f504aa7796e68504ee09c8a5394263f5a290de
Vagner Pereira <sousa.vagner82@gmail.com>
2020-08-17 15:39:11 -0300

    exe 5

--- exercises/7_3/exe5.js
+++ exercises/7_3/exe5.js
@@ -9,12 +9,18 @@ const obj2 = {
   description: 'My Description',
   title: 'My Title',
 };
-
 const obj3 = {
   title: 'My Different Title',
   description: 'My Description',
 };
+const calculator = {
+  add: (n1, n2) => Math.trunc(n1 + n2),
+  mult: (n1, n2) => Math.trunc(n1 * n2),
+  div: (n1, n2) => Math.trunc(n1 / n2),
+  sub: (n1, n2) => Math.trunc(n1 - n2),
+};
 
+console.log(calculator.add(1, 3));
 // implemente seus testes aqui
 assert.deepStrictEqual(obj1, obj2);
 assert.notDeepStrictEqual(obj1, obj3);
