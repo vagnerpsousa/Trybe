6fcb08507ea45fc10adfb80d672ce5226e588e98
Vagner Pereira <sousa.vagner82@gmail.com>
2021-01-05 15:26:53 -0300

    add

--- exercises/12_1/fancy-buttons/src/App.js
+++ exercises/12_1/fancy-buttons/src/App.js
@@ -1,42 +1,21 @@
 import React from 'react';
 import './App.css';
-//teste2
+import FancyButtons from './FancyButtons';
+import ColorFilter from './ColorFilter';
+import colors from './data.js';
+
 class App extends React.Component {
 
-  constructor() {
-    super()
-    this.handleClick1 = this.handleClick1.bind(this)
-    this.handleClick2 = this.handleClick2.bind(this)
-    this.handleClick3 = this.handleClick3.bind(this)
-    this.state = {
-      numeroDeCliques1: 0,
-      numeroDeCliques2: 0,
-      numeroDeCliques3: 0
-    }
-  }
-
-  handleClick1() {
-    this.setState((estadoAnterio, _props) => ({
-      numeroDeCliques1: estadoAnterio.numeroDeCliques1 + 1
-    }))
-  }
-  handleClick2() {
-    this.setState((estadoAnterio, _props) => ({
-      numeroDeCliques2: estadoAnterio.numeroDeCliques2 + 1
-    }))
-  }
-  handleClick3() {
-    this.setState((estadoAnterio, _props) => ({
-      numeroDeCliques3: estadoAnterio.numeroDeCliques3 + 1
-    }))
-  }
-
   render() {
-    return <div>
-      <button onClick={this.handleClick1}>{this.state.numeroDeCliques1}</button>
-      <button onClick={this.handleClick2}>{this.state.numeroDeCliques2}</button>
-      <button onClick={this.handleClick3}>{this.state.numeroDeCliques3}</button>
-    </div>
+    
+    return (
+      <div>
+        <FancyButtons />
+        <br/>
+        <ColorFilter colors={colors}/>
+      </div>
+    )
   }
+
 }
 export default App;
